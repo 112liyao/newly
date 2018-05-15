825148d7cd1b81b3940e02de60c7e3eb7a310eef
liyao <13920352045@163.com>
2018-05-15 21:07:03 +0800

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+function size(origin,type){
+	var type=type||"x";
+	var originw=origin;
+	if(type=="x"){
+		var client=$(window).width();
+	}else if(type=="y"){
+		var client=$(window).height();
+	}
+	var bili=client/originw*100+"px";
+	$("html").css("fontSize",bili);
+}
+$(document).ready(function(){
+	size(1334,'y');
+})
